97f2277975bccc128aba4e8156a008e4d467cd95
Sean D Gillespie <sean@mistersg.net>
2024-03-07 16:49:56 -0500

    chore(nix): Add cachix binary cache

--- flake.nix
+++ flake.nix
@@ -45,4 +45,18 @@
         packages.default = self'.packages.seandexer;
       };
     };
+
+  nixConfig = {
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "sgillespie.cachix.org-1:Zgif/WHW2IzHqbMb1z56cMmV5tLAA+zW9d5iB5w/VU4="
+    ];
+
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://sgillespie.cachix.org"
+    ];
+
+    allow-import-from-derivation = "true";
+    experimental-features = ["nix-command flakes"];
 }
